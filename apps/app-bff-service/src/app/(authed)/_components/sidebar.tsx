2a7eff6e6a750ea9cb67f6688d3a9d07aff6b670
ArraysID <arraysdotid@gmail.com>
2025-05-07 19:59:04 +0700

    refactor: improve sidebar layout for better structure and readability

--- apps/app-bff-service/src/app/(authed)/_components/sidebar.tsx
+++ apps/app-bff-service/src/app/(authed)/_components/sidebar.tsx
@@ -10,32 +10,38 @@ import { NavLink } from "./navlink";
 
 export function Sidebar() {
   return (
-    <aside className="h-[calc(100vh-4rem)] w-56 border-r">
-      <div className="flex flex-col px-2 pt-4">
-        <Button size="sm" variant="reverse" className="bg-white">
-          <FiPlus className="size-5" />
-          New post
-        </Button>
+    <aside className="flex h-[calc(100vh-4rem)] w-56 flex-col justify-between border-r">
+      <div>
+        <div className="flex flex-col px-2 pt-4">
+          <Button size="sm" variant="reverse" className="bg-white">
+            <FiPlus className="size-5" />
+            New post
+          </Button>
+        </div>
+
+        <nav className="pt-4">
+          <span className="px-3 text-xs">Menu Utama</span>
+          <NavLink
+            title="Beranda"
+            href="/"
+            icon={<MdOutlineExplore className="size-6" />}
+          />
+          <NavLink
+            title="Project"
+            href="/projects"
+            icon={<FiCodesandbox className="size-5" />}
+          />
+          <NavLink
+            title="Profil"
+            href="/profile"
+            icon={<CgProfile className="size-5" />}
+          />
+        </nav>
       </div>
 
-      <nav className="pt-4">
-        <span className="px-3 text-xs">Menu Utama</span>
-        <NavLink
-          title="Beranda"
-          href="/"
-          icon={<MdOutlineExplore className="size-6" />}
-        />
-        <NavLink
-          title="Project"
-          href="/projects"
-          icon={<FiCodesandbox className="size-5" />}
-        />
-        <NavLink
-          title="Profil"
-          href="/profile"
-          icon={<CgProfile className="size-5" />}
-        />
-      </nav>
+      <div className="text-muted-foreground border-t px-3 py-2 text-xs">
+        Version 0.0.1 (Alpha)
+      </div>
     </aside>
   );
 }
